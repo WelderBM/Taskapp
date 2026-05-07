d2e7006545e54efd8f6b2d3085212b2c920f97b7
welderbm <welderb40@gmail.com>
2026-05-07 15:02:24 -0400

    Move styles to a separate file for better organization and maintainability

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+import { styles } from "./styles"
 
 export default function App() {
   return (
@@ -10,21 +11,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#f2f4f7',
-  },
-  titulo: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#ff9500',
-    marginBottom: 4,
-  },
-  subtitulo: {
-    fontSize: 14,
-    color: '#777',
-  },
-});
---
+++ styles.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f2f4f7',
+  },
+  titulo: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#ff9500',
+    marginBottom: 4,
+  },
+  subtitulo: {
+    fontSize: 14,
+    color: '#777',
+  },
+});
